8194e01b65076b176295dd1257a2f154c38f88d7
Hildebrando Viana Matos <hildovianamatos17@gmail.com>
2021-01-28 21:56:37 -0300

    update

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,5 +1,19 @@
 $(function(){
     $('nav.mobile h3').click(function(){
-        $('nav.mobile ul').slideToggle();
-    })
+        var listaMenu = $('nav.mobile ul');
+        if(listaMenu.is(':hidden') == true){
+            var icone = $('h3').find('i');
+            icone.removeClass('fas fa-bars');
+            icone.addClass('fas fa-times');
+            listaMenu.slideToggle();
+        }
+        else{
+            var icone = $('h3').find('i');
+            icone.removeClass('fas fa-times');
+            icone.addClass('fas fa-bars');
+            listaMenu.slideToggle();
+        }
+    });
 })
+
+
